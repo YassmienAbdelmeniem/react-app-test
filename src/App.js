eb967e4b79a23b20f5342d85c9b6911f95e70a8b
YassmienAbdelmeniem <yassmien.yassin96@gmail.com>
2021-12-15 06:55:58 +0200

    first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import te, {x, y} from './module';
+// import {default as te} from './module';
+function App() {
+  return (
+    <div className="App">
+      hello world {x+y, te()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/module.js
@@ -0,0 +1,8 @@
+export const x = 2;
+export const y = 5;
+export function test(){
+    console.log("test");
+}
+export default function test2(){
+    console.log("test defult");
+}
